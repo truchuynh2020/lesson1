1f04a664971b2802de32c87442a42c43bbf212d2
truchuynh2020 <59849030+truchuynh2020@users.noreply.github.com>
2020-01-26 01:43:11 -0600

    Add files via upload

---
+++ Restaurant site 3/contactvalidate.js
@@ -0,0 +1,10 @@
+//only to display the message
+
+function validateItems() {
+  
+        alert("The provided information is valid. Thank you for contacting us. We will reply you soon.");
+        
+        return false;
+    }
+	
+	
